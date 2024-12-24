73d915430371100ce1109acb385dc81839a5daaf
Blacksnowz <flaxiaroz@gmail.com>
2024-12-24 14:17:19 +0700

    complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,48 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    
+    srand(time(0));
+
+    
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+   
+    for (int i = 0; i < 3; i++) {
+        cout << "Press Enter to continue..." << endl;
+        cin.ignore(); 
+    }
+
+    int randomNumber = rand() % 9 + 1;
+    string grade;
+
+   
+    if (randomNumber == 1) {
+        grade = "A";
+    } else if (randomNumber == 2) {
+        grade = "B+";
+    } else if (randomNumber == 3) {
+        grade = "B";
+    } else if (randomNumber == 4) {
+        grade = "C+";
+    } else if (randomNumber == 5) {
+        grade = "C";
+    } else if (randomNumber == 6) {
+        grade = "D+";
+    } else if (randomNumber == 7) {
+        grade = "D";
+    } else if (randomNumber == 8) {
+        grade = "F";
+    } else if (randomNumber == 9) {
+        grade = "W";
+    }
+
+
+    cout << "You will get " << grade << " in this 261102." << endl;
+
+    return 0;
+}
